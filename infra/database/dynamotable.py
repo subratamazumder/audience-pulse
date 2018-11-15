5ebb1927c63e4475bc5847c24369f45ba7c6d88e
Subrata Mazumder <subrata.besu@gmail.com>
2018-11-15 21:57:55 +0000

    question & stats table added

--- infra/database/dynamotable.py
+++ infra/database/dynamotable.py
@@ -3,16 +3,20 @@ from troposphere.dynamodb import Table, KeySchema, AttributeDefinition, Provisio
 
 PARAMETER_WRITE_CAPACITY = "WriteCapacityUnits"
 PARAMETER_READ_CAPACITY = "ReadCapacityUnits"
-TABLE_QUESTION = "QuestionTable"
 
 
 class AudiencePulseTable:
     t = Template()
+    TABLE_QUESTION = "QuestionTable"
+    TABLE_SESSION = "SessionTable"
+    TABLE_STATS = "StatsTable"
 
     def __init__(self):
         self.add_description()
         self.add_parameters()
-        self.create_question_table()
+        self.create_question_table(self.TABLE_QUESTION)
+        self.create_session_table(self.TABLE_SESSION)
+        self.create_stats_table(self.TABLE_STATS)
 
     def add_description(self):
         self.t.add_description("CFT of Dynamo tables for Audience Pulse application")
@@ -39,22 +43,9 @@ class AudiencePulseTable:
         self.t.add_parameter(rcu)
         self.t.add_parameter(wcu)
 
-    def create_question_table(self):
-        # "Properties" : {
-        #     "AttributeDefinitions" : [ AttributeDefinition, ... ],
-        #     "GlobalSecondaryIndexes" : [ GlobalSecondaryIndexes, ... ],
-        #     "KeySchema" : [ KeySchema, ... ],
-        #     "LocalSecondaryIndexes" : [ LocalSecondaryIndexes, ... ],
-        #     "PointInTimeRecoverySpecification" : PointInTimeRecoverySpecification,
-        #     "ProvisionedThroughput" : ProvisionedThroughput,
-        #     "SSESpecification" : SSESpecification,
-        #     "StreamSpecification" : StreamSpecification,
-        #     "TableName" : String,
-        #     "Tags" : [ Resource Tag, ... ],
-        #     "TimeToLiveSpecification" : TimeToLiveSpecification
-        #   }
+    def create_question_table(self, table_name):
         question_table = Table(
-            TABLE_QUESTION,
+            table_name,
             AttributeDefinitions=[
                 AttributeDefinition(
                     AttributeName="question_id",
@@ -93,6 +84,90 @@ class AudiencePulseTable:
             )
         )
         self.t.add_resource(question_table)
+
+    def create_session_table(self, table_name):
+        session_table = Table(
+            table_name,
+            AttributeDefinitions=[
+                AttributeDefinition(
+                    AttributeName="session_id",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="session_name",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="question_list",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="active_question_id",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="session_date",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="create_date",
+                    AttributeType="S"
+                )
+            ],
+            KeySchema=[
+                KeySchema(
+                    AttributeName="session_id",
+                    KeyType="HASH"
+                )
+            ],
+            ProvisionedThroughput=ProvisionedThroughput(
+                ReadCapacityUnits=Ref(PARAMETER_READ_CAPACITY),
+                WriteCapacityUnits=Ref(PARAMETER_WRITE_CAPACITY)
+            )
+        )
+        self.t.add_resource(session_table)
+
+    def create_stats_table(self, table_name):
+        stats_table = Table(
+            table_name,
+            AttributeDefinitions=[
+                AttributeDefinition(
+                    AttributeName="stat_id",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="session_id",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="question_id",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="no_of_answers",
+                    AttributeType="N"
+                ),
+                AttributeDefinition(
+                    AttributeName="answers",
+                    AttributeType="S"
+                ),
+                AttributeDefinition(
+                    AttributeName="create_date",
+                    AttributeType="S"
+                )
+            ],
+            KeySchema=[
+                KeySchema(
+                    AttributeName="stat_id",
+                    KeyType="HASH"
+                )
+            ],
+            ProvisionedThroughput=ProvisionedThroughput(
+                ReadCapacityUnits=Ref(PARAMETER_READ_CAPACITY),
+                WriteCapacityUnits=Ref(PARAMETER_WRITE_CAPACITY)
+            )
+        )
+        self.t.add_resource(stats_table)
 
     def get_cft(self):
         return self.t.to_json()
